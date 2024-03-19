95d670afb6cfea9e13f778552f559be068f1605d
Abdoulahi Fofana <abdoulahi.fofana@etu.univ-orleans.fr>
2024-03-19 18:09:23 +0100

    page de recherche

--- js/views/PersonnageAll.js
+++ js/views/PersonnageAll.js
@@ -1,31 +1,45 @@
 import PersoProvider from '../model/service/PersoProvider.js';
 
+
 export default class PersonnageAll {
     async render() {
         let personnages = await PersoProvider.fetchPerso(50);
         let html = personnages.map(perso =>
             /*html*/`
-            <div class="col">
-                <div class="card shadow-sm">
-                    <img src="${perso.image}" class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <div class="card-body">
-                        <p class="card-text">${perso.description ? perso.description.slice(0, 100) : ''}</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <a href="#/personnages/${perso.id}" class="btn btn-sm btn-outline-secondary">+ détail sur ${perso.nom}</a>
-                            </div>
-                            <small class="text-body-secondary">${perso.id}</small>
-                        </div>
+                <div class="card">
+                    <img src="${perso.image}" class="card-img" alt="..." loading="lazy">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">${perso.nom}</h5>
+                        <p class="card-text">z</p>
                     </div>
                 </div>
-            </div>
+
             `
         ).join('\n ');
 
         return /*html*/`
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                ${html}
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          
+
+          <div class="container text-center" id="searchBox">
+            <h1>wiki viewer</h1>
+            <div class="form col-xs-12">   
+                <input class="col-xs-9" id="searchBar" type="text" placeholder="search"/>
+                <span class="glyphicon glyphicon-search col-xs-1" data-toggle="tooltip" title="Search"></span>
+                <span class="glyphicon bar col-xs-1" id="submit"><b>|</b></span>
+                <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank"><span class="glyphicon glyphicon-random col-xs-1"  data-toggle="tooltip" title="Random topic"></span></a>
+              </div>
             </div>
+          </div>  
+          
+
+
+          <div class="d-flex">
+          ${html}
+          </div>
+          
+        </div>
+          
         `;
     }
 }
