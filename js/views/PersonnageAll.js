2e6c2a3ea702c7210c4afb46379197c4c630b6c2
Abdoulahi Fofana <abdoulahi.fofana@etu.univ-orleans.fr>
2024-03-19 18:47:08 +0100

    recherche

--- js/views/PersonnageAll.js
+++ js/views/PersonnageAll.js
@@ -3,13 +3,18 @@ import PersoProvider from '../model/service/PersoProvider.js';
 
 export default class PersonnageAll {
     async render() {
-      // Créer un input de recherche
-        let input = document.createElement('input');
-        input.setAttribute('type', 'text');
-        input.setAttribute('id', 'searchBar');
-        input.setAttribute('placeholder', 'search');
-        
+
+
         let personnages = await PersoProvider.fetchPerso(50);
+
+
+        const search = async () => {
+            let search = document.getElementById('searchBar').value;
+            console.log(search);
+            let personnages = await PersoProvider.rechercheNom(search);
+            console.log(personnages);
+        }
+
         let html = personnages.map(perso =>
             /*html*/`
                 <div class="card">
@@ -22,14 +27,16 @@ export default class PersonnageAll {
             `
         ).join('\n ');
 
+        console.log(search);
+
         return /*html*/`
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           
 
-          <form class="container text-center" id="searchBox" >
+          <form class="container text-center" id="searchBox"  >
             <h1>wiki viewer</h1>
             <div class="form col-xs-12">   
-                <input class="col-xs-9" id="searchBar" type="text" placeholder="search" onChange="${search()}"/>
+                <input class="col-xs-9" id="searchBar" type="text" placeholder="search" onChange="${search}"/>
                 <button class="btn btn-primary col-xs-2" id="searchButton">Search</button>
               </div>
             </div>
@@ -46,9 +53,6 @@ export default class PersonnageAll {
         `;
     }
 
-    async search(){
 
-    }
+
 }
-
-
