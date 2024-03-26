725880485e47d8407d16075bec50cd038bf9fb7a
Abdoulahi Fofana <abdoulahi.fofana@etu.univ-orleans.fr>
2024-03-26 11:56:34 +0100

    class favoris

--- js/views/PersoShow.js
+++ js/views/PersoShow.js
@@ -1,9 +1,11 @@
 import Utils from '../model/service/Utils.js';
+import MesFavoris from '../model/service/Favoris.js';
 import PersoProvider from '../model/service/PersoProvider.js';
 
 
 export default class PersoShow {
     async render () {
+        let lesFavoris 
         let request = Utils.parseRequestURL()
         console.log('request', request)
         const perso = await PersoProvider.getPerso(request.id)
@@ -15,6 +17,7 @@ export default class PersoShow {
             </section>
             <p><a href="/">Retour à l'accueil</a></p>
             <p><a href="#/personnages">Retour à la liste des personnages</a></p>
+            <img src="../../img/${personnage.image}" alt="${personnage.nom}">
         `
     }
 
